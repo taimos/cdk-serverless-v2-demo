89924aa8af4ccbfc2b37d47a3e856df05c4a0c54
Thorsten Hoeger <thorsten.hoeger@taimos.de>
2023-01-30 15:35:56 +0100

    chore: upgrade

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,7 +1,7 @@
 import { Datastore, RestApi, ServerlessProject, Workflow } from '@taimos/cdk-serverless-v2/lib/projen';
 
 const project = new ServerlessProject({
-  cdkVersion: '2.59.0',
+  cdkVersion: '2.60.0',
   defaultReleaseBranch: 'main',
   name: 'cdk-serverless-v2-demo',
   deps: [
